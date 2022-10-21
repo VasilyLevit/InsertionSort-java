d80c08b239ff485afe8f3064ebc1819618422c21
Vasily Levit <vasily.levit@gmail.com>
2022-10-21 22:11:20 +0800

    initial

---
+++ sort.java
@@ -0,0 +1,41 @@
+package InsertionSort;
+
+public class sort {
+    public void sort(int array[]) {
+        for (int left = 0; left < array.length; left++) {
+            // Вытаскиваем значение элемента
+            int value = array[left];
+            // Перемещаемся по элементам, которые перед вытащенным элементом
+            int i = left - 1;
+            for (; i >= 0; i--) {
+                // Если вытащили значение меньшее — передвигаем больший элемент дальше
+                if (value < array[i]) {
+                    array[i + 1] = array[i];
+                } else {
+                    // Если вытащенный элемент больше — останавливаемся
+                    break;
+                }
+            }
+            // В освободившееся место вставляем вытащенное значение
+            array[i + 1] = value;
+        }
+
+    }
+
+
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {12, 16, 13, 17, 22, 19};
+        printArray(arr);
+        sort object = new sort();
+        object.sort(arr);
+        System.out.print("Sorted array: ");
+        printArray(arr);
+    }
+}
